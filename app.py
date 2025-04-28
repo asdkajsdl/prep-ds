d64f61e5b27347a0cc7e714178711155bad5c677
Alexander Ayala Mejia <alexanderayalamejia@gmail.com>
2025-04-28 15:42:47 -0300

    hici dos rutas mas en flask

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/prep")
+def miauu():
+    return "<p>hola mundo!</p>"
+
+
+
+@app.route("/ds")
+def wauuu():
+    return "<p>argentina!</p>"
