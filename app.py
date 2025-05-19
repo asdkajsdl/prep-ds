5036f4ee70b507a3512f4e3d770aa5866d4ba74f
alexander <alexanderayalamejia@gmail.com>
2025-05-19 15:49:36 -0300

    hice rutas usando url_for y llevandi un link a otro

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"""
+    <p>hola prep!</p>
+    <a href='{url_for("wauuu", prep1="lele", prep2='lele')}'> ruta simple </a>
+    <a href="{ url_for('otra') }">Ruta simple</a>
+    <a href="{ url_for('miauu') }">Ruta simple</a>
+
+    """
 
 
 @app.route("/prep")
 def miauu():
-    return "<p>hola mundo!</p>"
+    return "<p>hola prep!</p>"
 
 
 
 @app.route("/ds/<string:prep1>/<string:prep2>")
 def wauuu(prep1,prep2):
     return f"{prep1} vs {prep2}"
+
+@app.route('/prepp')
+def index():
+    return render_template('wauuu')
+
+@app.route('/ds')
+def otra():
+    return render_template('lalalala')
+
+if __name__ == '__main__':
+    app.run(debug=True)
